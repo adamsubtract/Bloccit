dcec489bb79ac65a35f002fdf11ebe571ee604d6
Adam <adam@Adams-MacBook-Pro.local>
2019-04-23 10:24:43 -0700

    Current work

--- spec/integration/topics_spec.js
+++ spec/integration/topics_spec.js
@@ -4,15 +4,50 @@ const base = "http://localhost:3000/topics/";
 
 describe("routes : topics", () => {
 
-  describe("GET /topics", () => {
+      const sequelize = require("../../src/db/models/index").sequelize;
+      const Topic = require("../../src/db/models").Topic;
 
-    it("should return a status code 200", (done) => {
-      request.get(base, (err, res, body) => {
-        expect(res.statusCode).toBe(200);
-        done();
+      describe("routes : topics", () => {
+
+        //#2
+        beforeEach((done) => {
+          this.topic;
+          sequelize.sync({
+            force: true
+          }).then((res) => {
+
+            Topic.create({
+                title: "JS Frameworks",
+                description: "There is a lot of them"
+              })
+              .then((topic) => {
+                this.topic = topic;
+                done();
+              })
+              .catch((err) => {
+                console.log(err);
+                done();
+              });
+
+          });
+
+        });
+
+        describe("GET /topics", () => {
+          it("should return a status code 200 and all topics", (done) => {
+
+            //#3
+            request.get(base, (err, res, body) => {
+              expect(res.statusCode).toBe(200);
+              expect(err).toBeNull();
+              expect(body).toContain("Topics");
+              expect(body).toContain("JS Frameworks");
+              done();
+            });
+          });
+        });
+
+
+
       });
-    });
-
-  });
-
 });
--- src/db/models/banner.js
+++ src/db/models/banner.js
@@ -2,14 +2,23 @@
 module.exports = (sequelize, DataTypes) => {
   const Banner = sequelize.define('Banner', {
     source: DataTypes.STRING,
-    description: DataTypes.STRING
+    description: DataTypes.STRING,
+    topicId: {
+      type: DataTypes.INTEGER,
+      onDelete: "CASCADE",
+      references: {
+        model: "Topics",
+        key: "id",
+        as: "topicId",
+      }
+    }
   }, {});
   Banner.associate = function(models) {
     // associations can be defined here
     Banner.belongsTo(models.Topic, {
-       foreignKey: "topicId",
-       onDelete: "CASCADE",
-     });
+      foreignKey: "topicId",
+      onDelete: "CASCADE",
+    });
   };
   return Banner;
 };
