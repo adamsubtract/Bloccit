c88024f95d49d5494ca4277de64acd406f390e7d
Adam <adam@Adams-MacBook-Pro.local>
2019-04-25 19:51:46 -0700

    Current work

--- src/db/queries.topics.js
+++ src/db/queries.topics.js
@@ -5,7 +5,6 @@ module.exports = {
 //#1
   getAllTopics(callback){
     return Topic.findAll()
-
 //#2
     .then((topics) => {
       callback(null, topics);
@@ -13,5 +12,56 @@ module.exports = {
     .catch((err) => {
       callback(err);
     })
+  },
+  addTopic(newTopic, callback){
+      return Topic.create({
+        title: newTopic.title,
+        description: newTopic.description
+      })
+      .then((topic) => {
+        callback(null, topic);
+      })
+      .catch((err) => {
+        callback(err);
+      })
+  },
+  getTopic(id, callback){
+     return Topic.findByPk(id)
+     .then((topic) => {
+       callback(null, topic);
+     })
+     .catch((err) => {
+       callback(err);
+     })
+  },
+  deleteTopic(id, callback){
+    return Topic.destroy({
+      where: {id}
+    })
+    .then((topic) => {
+      callback(null, topic);
+    })
+    .catch((err) => {
+      callback(err);
+    })
+  },
+  updateTopic(id, updatedTopic, callback){
+    return Topic.findById(id)
+    .then((topic) => {
+      if(!topic){
+        return callback("Topic not found");
+      }
+
+//#1
+      topic.update(updatedTopic, {
+        fields: Object.keys(updatedTopic)
+      })
+      .then(() => {
+        callback(null, topic);
+      })
+      .catch((err) => {
+        callback(err);
+      });
+    });
   }
 }
