3c2b72aa6930c1548bebf9fca378e264cf2805e6
Adam <adam@Adams-MacBook-Pro.local>
2019-05-01 14:27:41 -0700

    Current work

--- spec/integration/posts_spec.js
+++ spec/integration/posts_spec.js
@@ -54,6 +54,34 @@ describe("routes : posts", () => {
 
   describe("POST /topics/:topicId/posts/create", () => {
 
+    it("should not create a new post that fails validations", (done) => {
+      const options = {
+        url: `${base}/${this.topic.id}/posts/create`,
+        form: {
+
+//#1
+          title: "a",
+          body: "b"
+        }
+      };
+
+      request.post(options,
+        (err, res, body) => {
+
+//#2
+          Post.findOne({where: {title: "a"}})
+          .then((post) => {
+              expect(post).toBeNull();
+              done();
+          })
+          .catch((err) => {
+            console.log(err);
+            done();
+          });
+        }
+      );
+    });
+
     it("should create a new post and redirect", (done) => {
        const options = {
          url: `${base}/${this.topic.id}/posts/create`,
