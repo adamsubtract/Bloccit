a8c7c85869a28aed992431300209ac014a5321aa
Adam <adam@Adams-MacBook-Pro.local>
2019-05-03 15:21:10 -0700

    Merging files

--- src/config/main-config.js
+++ src/config/main-config.js
@@ -7,28 +7,20 @@ const session = require("express-session");
 const flash = require("express-flash");
 
 module.exports = {
-<<<<<<< HEAD
-  init(app, express){
+  init(app, express) {
     app.set("views", viewsFolder);
     app.set("view engine", "ejs");
     app.use(bodyParser.urlencoded({ extended: true }));
     app.use(expressValidator());
-    app.use(session({
-      secret: process.env.cookieSecret,
-      resave: false,
-      saveUninitialized: false,
-      cookie: { maxAge: 60000 }
-    }));
+    app.use(
+      session({
+        secret: process.env.cookieSecret,
+        resave: false,
+        saveUninitialized: false,
+        cookie: { maxAge: 60000 }
+      })
+    );
     app.use(flash());
     app.use(express.static(path.join(__dirname, "..", "assets")));
   }
 };
-=======
-    init(app, express){
-        app.set("views", viewsFolder);
-        app.set("view engine", "ejs");
-        app.use(bodyParser.urlencoded({ extended: true }));
-        app.use(express.static(path.join(__dirname, "..", "assets")));
-    }
-};
->>>>>>> assignment-post-resource-2
