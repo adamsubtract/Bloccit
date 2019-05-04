1808132a4cf777d390d77ae0c03cacb1dd3aab71
Adam <adam@Adams-MacBook-Pro.local>
2019-05-03 19:50:59 -0700

    checkpoint authentication

--- src/config/route-config.js
+++ src/config/route-config.js
@@ -1,13 +1,15 @@
 module.exports = {
-    init(app){
-        const staticRoutes = require("../routes/static");
-        const topicRoutes = require("../routes/topics");
-        const postRoutes = require("../routes/posts");
-        const flairRoutes = require("../routes/flairs");
+  init(app) {
+    const staticRoutes = require("../routes/static");
+    const topicRoutes = require("../routes/topics");
+    const postRoutes = require("../routes/posts");
+    const userRoutes = require("../routes/users");
+    const flairRoutes = require("../routes/flairs");
 
-        app.use(staticRoutes);
-        app.use(topicRoutes);
-        app.use(postRoutes);
-        app.use(flairRoutes);
-    }
-}
+    app.use(staticRoutes);
+    app.use(topicRoutes);
+    app.use(postRoutes);
+    app.use(userRoutes);
+    app.use(flairRoutes);
+  }
+};
