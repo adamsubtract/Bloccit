3e6c871dfdb0763dae48d7edc0b5113c8ce4bba2
Adam <adam@Adams-MacBook-Pro.local>
2019-05-04 13:45:01 -0700

    Current work

--- spec/unit/topic_spec.js
+++ spec/unit/topic_spec.js
@@ -1,98 +1,104 @@
 const sequelize = require("../../src/db/models/index").sequelize;
 const Topic = require("../../src/db/models").Topic;
 const Post = require("../../src/db/models").Post;
+const User = require("../../src/db/models").User;
 
 describe("Post", () => {
-
-  beforeEach((done) => {
-//#1
+  beforeEach(done => {
     this.topic;
     this.post;
-    sequelize.sync({force: true}).then((res) => {
+    this.user;
 
-//#2
-      Topic.create({
-        title: "Batman dies in Avengers Endgame",
-        description: "His gadgets were not enough for Thanos"
-      })
-      .then((topic) => {
-        this.topic = topic;
-//#3
-        Post.create({
-          title: "Im sad",
-          body: "I cried at the movie theater",
-//#4
-          topicId: this.topic.id
-        })
-        .then((post) => {
-          this.post = post;
+    sequelize.sync({ force: true }).then(res => {
+      // #2
+      User.create({
+        email: "starman@tesla.com",
+        password: "Trekkie4lyfe"
+      }).then(user => {
+        this.user = user; //store the user
+
+        // #3
+        Topic.create(
+          {
+            title: "Expeditions to Alpha Centauri",
+            description:
+              "A compilation of reports from recent visits to the star system.",
+
+            // #4
+            posts: [
+              {
+                title: "My first visit to Proxima Centauri b",
+                body: "I saw some rocks.",
+                userId: this.user.id
+              }
+            ]
+          },
+          {
+            // #5
+            include: {
+              model: Post,
+              as: "posts"
+            }
+          }
+        ).then(topic => {
+          this.topic = topic; //store the topic
+          this.post = topic.posts[0]; //store the post
           done();
         });
-      })
-      .catch((err) => {
-        console.log(err);
-        done();
       });
     });
+  });
 
-   });
-
-   describe("#create()", () => {
-
-        it("when calling Topic.create, that a topic is created and stored in database", (done) => {
-            Topic.create({
-                title: "Coding is hard",
-                description: "My head hurts"
-            })
-            .then((topic) => {
-                expect(topic.title).toBe("Coding is hard");
-                expect(topic.description).toBe("My head hurts");
-                done();
-            })
-            .catch((err) => {
-                console.log(err);
-                done();
-            });
+  describe("#create()", () => {
+    it("when calling Topic.create, that a topic is created and stored in database", done => {
+      Topic.create({
+        title: "Coding is hard",
+        description: "My head hurts"
+      })
+        .then(topic => {
+          expect(topic.title).toBe("Coding is hard");
+          expect(topic.description).toBe("My head hurts");
+          done();
+        })
+        .catch(err => {
+          console.log(err);
+          done();
         });
+    });
 
-        it(("should not create a topic with missing title, or description"), (done)=> {
-            Topic.create({
-
-            })
-            .then((topic) => {
-                done();
-            })
-            .catch((err) => {
-                expect(err.message).toContain("Topic.title cannot be null");
-                expect(err.message).toContain("Topic.description cannot be null");
-                done();
-            });
-
+    it("should not create a topic with missing title, or description", done => {
+      Topic.create({})
+        .then(topic => {
+          done();
+        })
+        .catch(err => {
+          expect(err.message).toContain("Topic.title cannot be null");
+          expect(err.message).toContain("Topic.description cannot be null");
+          done();
         });
+    });
+  });
 
-   });
-
-   describe("#getPosts()", () => {
-    
-    it("should return the associated topic", (done) => {
-    Post.create({
+  describe("#getPosts()", () => {
+    it("should return the associated topic", done => {
+      Post.create({
         title: "Batman faked death and moved to Wakanda",
         body: "Batman has been seen in Wakanda rasing family with Wonder Woman",
         topicId: this.topic.id
-    })
-    .then((posts) => {
-        this.topic.getPosts()
-        .then((posts) => {
-            expect(posts[0].title).toBe("Im sad")
-            expect(posts[1].title).toBe("Batman faked death and moved to Wakanda")
+      })
+        .then(posts => {
+          this.topic.getPosts().then(posts => {
+            expect(posts[0].title).toBe("Im sad");
+            expect(posts[1].title).toBe(
+              "Batman faked death and moved to Wakanda"
+            );
             done();
+          });
         })
-    })
-    .catch((err) => {
-        console.log(err);
-        done();
+        .catch(err => {
+          console.log(err);
+          done();
+        });
     });
-
-    });
-   });
+  });
 });
--- src/db/models/post.js
+++ src/db/models/post.js
@@ -1,23 +1,35 @@
-'use strict';
+"use strict";
 module.exports = (sequelize, DataTypes) => {
-  const Post = sequelize.define('Post', {
-    title: {
-      type: DataTypes.STRING,
-      allowNull: false
+  const Post = sequelize.define(
+    "Post",
+    {
+      title: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      body: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      topicId: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+      },
+      userId: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+      }
     },
-    body: {
-      type: DataTypes.STRING,
-      allowNull: false
-    },
-    topicId: {
-      type: DataTypes.INTEGER,
-      allowNull: false
-    }
-  }, {});
+    {}
+  );
   Post.associate = function(models) {
     // associations can be defined here
     Post.belongsTo(models.Topic, {
       foreignKey: "topicId",
+      onDelete: "CASCADE"
+    });
+    Post.belongsTo(models.User, {
+      foreignKey: "userId",
       onDelete: "CASCADE"
     });
   };
