38b2118df683f2a1eb1414aec52fdeb7d4a5d3d8
Adam <adam@Adams-MacBook-Pro.local>
2019-05-11 00:12:20 -0700

    Assignment favoriting

--- src/db/queries.votes.js
+++ src/db/queries.votes.js
@@ -39,5 +39,24 @@ module.exports = {
           });
       }
     });
+  },
+  hasUpvoteFor(userId, callback) {
+    return Vote.findById(userId).then(vote => {
+      if (vote.value === 1) {
+        return callback(true);
+      } else {
+        return callback(false);
+      }
+    });
+  },
+
+  hasDownvoteFor(userId, callback) {
+    return Vote.findById(userId).then(vote => {
+      if (vote.value === -1) {
+        return callback(true);
+      } else {
+        return callback(false);
+      }
+    });
   }
 };
